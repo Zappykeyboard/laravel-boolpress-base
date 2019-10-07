8888413cd58960792384d36d2888d047938db40a
Edoardo <a@Air-di-edo.lan>
2019-10-07 16:24:00 +0200

    creato seeder Post

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,15 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Model;
+use Faker\Generator as Faker;
+use App\Post;
+
+$factory->define(Post::class, function (Faker $faker) {
+    return [
+      'author'=> $faker->userName,
+      'body'=> $faker->text,
+      'creation_date'=> $faker->dateTimeThisDecade,
+    ];
+});
