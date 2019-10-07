ddc7e239bc0fae001b46aae63082365ef61fe714
Edoardo <a@Air-di-edo.lan>
2019-10-07 17:28:28 +0200

    aggiunta join (non funzionante)

--- app/Post.php
+++ app/Post.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    protected $fillable=[
+      'author',
+      'content',
+      'creation_date'
+    ];
+
+    public function category(){
+      return $this->belongsTo(Category::class);
+    }
 }
---
+++ database/migrations/2019_10_07_151333_add_foreign_key.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddForeignKey extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('posts', function (Blueprint $table){
+
+        $table->bigInteger('category_id')->unsigned()->index();
+        $table->foreign('category_id','category')
+              ->references('id')
+              ->on('categories');
+      });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('posts',function (Blueprint $table){
+          $table->dropForeign('category');
+          $table->dropColumn('category_id');
+        });
+    }
+}
