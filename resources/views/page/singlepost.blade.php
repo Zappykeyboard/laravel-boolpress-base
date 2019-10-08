76616b7312d88ba4931344d51bb8187016118e82
Edoardo <a@Air-di-edo.lan>
2019-10-08 18:07:51 +0200

    modificata struttura singlepost

--- resources/views/page/singlepost.blade.php
+++ resources/views/page/singlepost.blade.php
@@ -1,14 +1,20 @@
 @extends('layout.base')
 
 @section('content')
+  <div class="post">
+    <p>Title: {{$post->title}}</p>
+    <p>By: {{$post-> author}}</p>
+    <p>Created at: {{$post->creation_date}}</p>
+    <p>Category: {{$catName}}</p>
+    <button type="button" name="button">
+      <a href="{{route('post.edit', $post->id)}}">Edit</a>
+    </button>
+    <button type="button" name="button">
+      <a href="{{route('post.destroy', $post->id)}}">!!DELETE!!</a>
+    </button>
 
-  <p>Title: {{$post->title}}</p>
-  <p>By: {{$post-> author}}</p>
-  <p>Created at: {{$post->creation_date}}</p>
-  <p>Category: {{$catName}}</p>
-  <a href="{{route('post.edit', $post->id)}}">Edit</a>
-  -----------------------------
-  <a href="{{route('post.destroy', $post->id)}}">!!DELETE!!</a>
-  <p>{!!$post->content!!}</p>
+    <p>{!!$post->content!!}</p>
+
+  </div>
 
 @endsection
